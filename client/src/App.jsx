8db8592305aa8fd4ae506a9560a2d210c34e0f48
Roohith18@ <roohithbalag.23csd@kongu.edu>
2026-06-16 15:18:57 +0530

    Initial Comit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {User} from './User'
+import { User1 } from './User'
+import About from './About'
+const App = () => {
+  return (
+    <div>
+      <User/>
+      <User1/>
+      <About></About>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/User.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+      <p>User</p>
+    </div>
+  )
+}
+const User1=()=>{
+  return(
+    <div>
+      <p>Prakash</p>
+    </div>
+  )
+}
+
+export  {User,User1};
