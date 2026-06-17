85fc32b4658795b633cd055fc6ee444b6fcfef8c
Roohith Bala <roohithbalag.23csd@kongu.edu>
2026-06-17 15:00:52 +0530

    Conditional Rendering

--- conditional-rendering/src/App.jsx
+++ conditional-rendering/src/App.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const App = () => {
-const loggedIn=false;
-if(loggedIn){
-  return (
-    <div><h1>App</h1></div>
-  )
-}
+  const [user,setUser]=useState([]);
+  const loggedIn=false;
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(data=>data.json())
+    .then(data=>setUser(data));
+  },[]);
+  if(!loggedIn){
+   return (
+    <div>
+      {user.map((user) => (
+        <h3 key={user.id}>{user.name}</h3>
+      ))}
+    </div>
+  );
+};
+
+
 return(
   <div><h1>Prakash</h1></div>
 )
