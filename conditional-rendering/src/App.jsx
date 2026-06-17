a38902256c0f8da0686d5983f07b943aec8f8d45
Roohith Bala <roohithbalag.23csd@kongu.edu>
2026-06-17 14:31:39 +0530

    Conditional Rendering

---
+++ conditional-rendering/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+const loggedIn=false;
+if(loggedIn){
+  return (
+    <div><h1>App</h1></div>
+  )
+}
+return(
+  <div><h1>Prakash</h1></div>
+)
+}
+export default App
